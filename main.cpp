f2b1032fafb642d738248b0fedb8134ebc3b56a8
martiinbra <54995790+martiinbra@users.noreply.github.com>
2019-09-06 12:07:00 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a , b, suma;
+    cout << "ingrese dos numeros";
+    cin >> a >> b;
+
+    suma = a + b;
+
+    cout << suma;
+
+    return 0;
+
+
+}
+
+
+
+
+
